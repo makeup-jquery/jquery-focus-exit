60c1a427a9fa11ec1472c7c8d0e1f0dcfef69401
Ian McBurnie <imcburnie@ebay.com>
2016-01-06 09:48:33 -0800

    Prevent each element from receiving plugin more than once

--- jquery.focusexit.js
+++ jquery.focusexit.js
@@ -1,6 +1,6 @@
 /**
 * @function jquery.focusexit.js
-* @version 0.0.5
+* @version 0.0.6
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
 * @desc Triggers 'focusexit' event when keyboard focus has completely left the element.
 * @fires {object} focusexit
@@ -9,40 +9,48 @@
 */
 (function ($, window, document, undefined) {
 
+    var pluginName = 'jquery-focus-exit';
+
     $.fn.focusExit = function focusExit() {
 
         return this.each(function onEach() {
 
-            var $this = $(this),
-                relatedTargetShim,
-                timeout;
+            // check plugin does not already exist
+            if (!$.data(this, pluginName)) {
 
-            function onElementFocusIn() {
-                window.clearTimeout(timeout);
+                jQuery.data(this, pluginName, 'true');
+
+                var $this = $(this),
+                    relatedTargetShim,
+                    timeout;
+
+                function onElementFocusIn() {
+                    window.clearTimeout(timeout);
+                }
+
+                function onDocumentFocusIn(e) {
+                    relatedTargetShim = e.target;
+                }
+
+                // KeyboardEvent.relatedTarget is not supported in Firefox.
+                // So we manually listen to see where focus goes to in the doc.
+                $this.on('focusin', function() {
+                    $(document).on('focusin', onDocumentFocusIn);
+                });
+
+                $this.on('focusout', function onFocusOut(e) {
+                    timeout = window.setTimeout(function onTimeout() {
+                        $(document).off('focusin', onDocumentFocusIn);
+                        $this.off('focusin', onElementFocusIn);
+                        $this.trigger('focusexit', {
+                            "lostFocus": e.target,
+                            "gainedFocus": e.relatedTarget || relatedTargetShim
+                        });
+                    }, 100);
+
+                    $this.one('focusin', onElementFocusIn);
+                });
             }
-
-            function onDocumentFocusIn(e) {
-                relatedTargetShim = e.target;
-            }
-
-            // KeyboardEvent.relatedTarget is not supported in Firefox.
-            // So we manually listen to see where focus goes to in the doc.
-            $this.on('focusin', function() {
-                $(document).on('focusin', onDocumentFocusIn);
-            });
-
-            $this.on('focusout', function onFocusOut(e) {
-                timeout = window.setTimeout(function onTimeout() {
-                    $(document).off('focusin', onDocumentFocusIn);
-                    $this.off('focusin', onElementFocusIn);
-                    $this.trigger('focusexit', {
-                        "lostFocus": e.target,
-                        "gainedFocus": e.relatedTarget || relatedTargetShim
-                    });
-                }, 100);
-
-                $this.one('focusin', onElementFocusIn);
-            });
         });
 
     };
