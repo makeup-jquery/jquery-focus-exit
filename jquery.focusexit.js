cd2c23b4fe729424eb6ca3bd50937a8c78c6202c
Ian McBurnie <imcburnie@ebay.com>
2016-03-16 08:56:01 -0700

    Added main file ref

--- jquery.focusexit.js
+++ jquery.focusexit.js
@@ -1,6 +1,6 @@
 /**
 * @function jquery.focusexit.js
-* @version 0.1.1
+* @version 0.1.2
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
 * @desc Triggers 'focusExit' event when keyboard focus has completely left the element.
 * @fires {object} focusExit
